94b4a631b1c746c6db50af6a34f91451bce6f46b
evren.kanalici <evrrren@gmail.com>
2016-08-07 09:50:49 +0300

    - concurrent paged reading for FASTQ format

--- KmerFreq/QRead.cpp
+++ KmerFreq/QRead.cpp
@@ -71,6 +71,8 @@ void QRead::check_file()
 
 void QRead::process_read(const string &line, unsigned kmersize)
 {
+    lock_guard<mutex> kmers_locker(kmers_mutex_);
+    
     for(int i=0; i<line.length()-kmersize+1; ++i)
     {
         string kmer = line.substr(i, kmersize);
@@ -84,6 +86,79 @@ void QRead::process_read(const string &line, unsigned kmersize)
             slot->second += 1;
         }
     }
+}
+
+
+//(n+1) offsets
+vector<size_t> QRead::qoffset_ranges(char *map, size_t file_len, unsigned n)
+{
+    vector<size_t> offsets;
+    offsets.push_back(0);
+    for(int i=0; i<n-1; ++i)
+    {
+        size_t offset = (i+1)*(file_len/n);
+        while(map[offset] != '@')
+            offset -= 1;
+        offsets.push_back(offset);
+    }
+    offsets.push_back(file_len);
+    
+    return std::move(offsets);
+}
+
+void QRead::qrange_task(char *map, size_t start, size_t end, unsigned kmersize)
+{
+    unsigned long offset = 0;
+    unsigned long cur = 0;
+    char buf_line[MAXSIZE_READ+1];
+    
+    cout << "starting task: " << this_thread::get_id() << "range - " << start << ":" << end << endl;
+    
+    char block_offset = 0;
+    short line_offset = 0;
+    int idx_read = 0;
+    for(offset = start; offset < end; ++offset)
+    {
+        cur = map[offset];
+        if(cur == '\n')
+        {
+            block_offset = (block_offset + 1) % 4;
+            line_offset = -1;
+        }
+        else
+        {
+            line_offset++;
+        }
+        
+        if(block_offset == 1)
+        {
+            
+            if(line_offset == 0)
+            {
+                memset(buf_line, '\0', (MAXSIZE_READ+1));
+            }
+            
+            if(line_offset > MAXSIZE_READ)
+            {
+                handle_error("MAXSIZE_READ reached!");
+            }
+            buf_line[line_offset] = (char)cur;
+            
+        }
+        else if(block_offset == 2 && line_offset == 0) // sequence read
+        {
+            string line(buf_line);
+#if DEBUG
+            cout << "+ read-" << ++idx_read << ": " << flush;
+            cout << line << endl;
+#endif
+            process_read(line, kmersize);
+        }
+        else {
+            //TODO: determistic skip?
+        }
+    }
+    
 }
 
 
@@ -117,12 +192,16 @@ void QRead::generate_paged(unsigned kmersize)
     int fd = open(filename_.c_str(), O_RDONLY);
     
     if (fd == -1)
+    {
         handle_error("error opening file!");
-    if (fstat(fd, &sb) == -1)           
+    }
+    if (fstat(fd, &sb) == -1)
+    {
+        close(fd);
         handle_error("error retrieving file!");
+    }
     
     unsigned long file_len = (unsigned long)lseek(fd, 0, SEEK_END);
-    
     
     //page map
     char *map = (char*)mmap(NULL, file_len, PROT_READ, MAP_SHARED, fd, 0);
@@ -131,59 +210,25 @@ void QRead::generate_paged(unsigned kmersize)
         handle_error("Error mmapping the file");
     }
     
-    unsigned long offset = 0;
-    unsigned long cur = 0;
-    char buf_line[MAXSIZE_READ+1];
     
-    // We only care about the 2nd line of each 4-line block, so some bookkeeping is in order.
-    char block_offset = 0;
-    short line_offset = 0;
-    int idx_read = 0;
-    for(offset = 0; offset < file_len; ++offset)
+    auto ranges = qoffset_ranges(map, file_len, cores_);
+    vector<thread> tasks;
+    for(auto it = ranges.begin(); it != ranges.end()-1; ++it)
     {
-        cur = map[offset];
-        if(cur == '\n')
-        {
-            block_offset = (block_offset + 1) % 4;
-            line_offset = -1;
-        }
-        else
-        {
-            line_offset++;
-        }
+        size_t start = *it;
+        size_t end = *(it+1);
+        cout << "tasking range: " << start << ":" << end << endl;
         
-        if(block_offset == 1)
-        {
-            
-            if(line_offset == 0)
-            {
-                memset(buf_line, '\0', (MAXSIZE_READ+1));
-            }
-            
-            if(line_offset > MAXSIZE_READ)
-            {
-                close(fd);
-                munmap(map, file_len);
-                handle_error("MAXSIZE_READ reached!");
-            }
-            buf_line[line_offset] = (char)cur;
-            
-        }
-        else if(block_offset == 2 && line_offset == 0) // sequence read
-        {
-            string line(buf_line);
-#if DEBUG
-            cout << "+ read-" << ++idx_read << ": ";
-            cout << line << endl;
-#endif
-            process_read(line, kmersize);
-        }
-        else {
-            //TODO: determistic skip?
-        }
+        tasks.emplace_back(&QRead::qrange_task, this, map, start, end, kmersize);
+        
     }
-    close(fd);
+    for (auto& t : tasks) {
+        t.join();
+    }
+    
     munmap(map, file_len);
+    close(fd);
+    
 
 }
 
--- KmerFreq/QRead.hpp
+++ KmerFreq/QRead.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <map>
 #include <unordered_map>
+#include <vector>
 #include <thread>
 
 class QRead
@@ -22,11 +23,14 @@ private:
     size_t readlen_;
     bool bad_file_;
     std::once_flag check_file_flag_;
+    std::mutex kmers_mutex_;
     std::unordered_map<std::string, unsigned> kmers_;
     
     
     void check_file(void);
     void process_read(const std::string &line, unsigned kmersize);
+    std::vector<size_t> qoffset_ranges(char *map, size_t file_len, unsigned n);
+    void qrange_task(char *map, size_t start, size_t end, unsigned kmersize);
     void generate(unsigned kmersize);
     void generate_paged(unsigned kmersize);
     
